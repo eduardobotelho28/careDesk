0536b43d3a70693f19e643cee669f540716402ec
= <=>
2024-12-01 22:26:19 -0300

    Adicionando novos arquivos e pastas

---
+++ View/src/pages/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Caredesk Login</title>
+    <link rel="stylesheet" href="../assets/css/login.css">
+    <link rel="stylesheet" href="../assets/css/syle.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="../assets/images/favicon.ico">
+</head>
+<body>
+    <div class="grid-container">
+        <div class="apresentantion-container">
+            <div class="content">
+                <h1 class="title">CAREDESK</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+        </div>
+        <div class="login-container">
+        <img class="logo" src="../assets/images/logo-caredesk.png" alt="Logo CAREDESK">
+            <h1>LOG IN</h1>
+            <input type="text" placeholder="CPF">
+            <input type="password" placeholder="Password">
+            <button type="submit">Log in</button>
+            <a href="#">Forgot your password?</a>
+        </div>
+    </div>
+</body>
+</html>
