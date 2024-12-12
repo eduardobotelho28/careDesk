40b7499432a94f51387da7e2296a8d191bffa581
Eduardo Botelho <126706543+eduardobotelho28@users.noreply.github.com>
2024-12-12 11:24:42 -0300

    começando o controller de medicos

---
+++ Controllers/MedicoController.php
@@ -0,0 +1,53 @@
+<?php
+// require_once '../Models/UserModel.php';
+
+header('Content-Type: application/json; charset=utf-8');
+
+$controller = new MedicoController();
+$controller->processRequest();
+
+class MedicoController {
+
+    // public $model;
+
+    // public function __construct() {
+    //     $this->model = new UserModel();
+    // }
+
+    //método que faz o roteamento
+    public function processRequest() {
+
+        $action = $_GET['action'];
+
+        switch ($action) {
+            case 'register':
+                $this->register();
+                break;
+            
+            default:
+                http_response_code(404);
+                die(json_encode('ação não existente no controoler'));
+                break;
+        }
+
+    }
+
+    private function register () {
+
+    }
+
+    private function valid_input_data ($data) {
+
+    }
+
+    function sanitize_data($data) {
+    }
+
+    function valid_method ($method) {
+        if($_SERVER['REQUEST_METHOD'] != $method)  {
+            die(json_encode('nao permitido'));
+        }
+    }
+    
+
+}
